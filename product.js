42693911ca78a82104ef33339e3901d8aaf16b2e
Gayathri M <gayathri1063@gmail.com>
2025-02-03 16:09:55 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,649 @@
+//T3 ultra
+function T3img1()
+	{
+		var img = document.getElementById('T3 Ultra Img');
+		img.src = 'Images/Product/T3 Ultra Image1.webp'
+	}
+function T3img2()
+	{
+		var img = document.getElementById('T3 Ultra Img');
+		img.src = 'Images/Product/T3 Ultra Image2.webp'
+	}   
+//T3 pro 5G
+function T3proimg1()
+	{
+		var img = document.getElementById('T3 pro 5G Img');
+		img.src = 'Images/Product/T3 Pro 5G Image1.webp'
+	}
+function T3proimg2()
+	{
+		var img = document.getElementById('T3 pro 5G Img');
+		img.src = 'Images/Product/T3 Pro 5G Image2.webp'
+	}
+//V40
+function V40img1()
+	{
+		var img = document.getElementById('V40 Img');
+		img.src = 'Images/Product/V40 Image1.webp'
+	}
+function V40img2()
+	{
+		var img = document.getElementById('V40 Img');
+		img.src = 'Images/Product/V40 Image2.webp'
+	}
+function V40img3()
+	{
+		var img = document.getElementById('V40 Img');
+		img.src = 'Images/Product/V40 Image3.webp'
+	}	
+//V40 Pro
+function V40proimg1()
+	{
+		var img = document.getElementById('V40 Pro Img');
+		img.src = 'Images/Product/V40 Pro Image1.webp'
+	}
+function V40proimg2()
+	{
+		var img = document.getElementById('V40 Pro Img');
+		img.src = 'Images/Product/V40 Pro Image2.webp'
+	}
+//Y28e 5G
+function Y28e5Gimg1()
+	{
+		var img = document.getElementById('Y28e 5G Img');
+		img.src = 'Images/Product/Y28e 5G Image1.webp'
+	}
+function Y28e5Gimg2()
+	{
+		var img = document.getElementById('Y28e 5G Img');
+		img.src = 'Images/Product/Y28e 5G Image2.webp'
+	}	
+//Y28s 5G
+function Y28s5Gimg1()
+	{
+		var img = document.getElementById('Y28s 5G Img');
+		img.src = 'Images/Product/Y28s 5G Image1.webp'
+	}
+function Y28s5Gimg2()
+	{
+		var img = document.getElementById('Y28s 5G Img');
+		img.src = 'Images/Product/Y28s 5G Image2.webp'
+	}
+//T3 Lite 5G
+function T3lite5gimg1()
+	{
+		var img = document.getElementById('T3 Lite 5G Img');
+		img.src = 'Images/Product/T3 Lite 5G Image1.webp'
+	}
+function T3lite5gimg2()
+	{
+		var img = document.getElementById('T3 Lite 5G Img');
+		img.src = 'Images/Product/T3 Lite 5G Image2.webp'
+	}	
+//Y58 5G
+function Y585Gimg1()
+	{
+		var img = document.getElementById('Y58 5G Img');
+		img.src = 'Images/Product/Y58 5G Image1.webp'
+	}
+function Y585Gimg2()
+	{
+		var img = document.getElementById('Y58 5G Img');
+		img.src = 'Images/Product/Y58 5G Image2.webp'
+	}				
+//Y200 Pro
+function Y200Proimg1()
+	{
+		var img = document.getElementById('Y200 Pro Img');
+		img.src = 'Images/Product/Y200 Pro 5G Image1.webp'
+	}
+function Y200Proimg2()
+	{
+		var img = document.getElementById('Y200 Pro Img');
+		img.src = 'Images/Product/Y200 Pro 5G Image2.webp'
+	}
+//V30e
+function V30eimg1()
+	{
+		var img = document.getElementById('V30e Img');
+		img.src = 'Images/Product/V30e Image1.webp'
+	}
+function V30eimg2()
+	{
+		var img = document.getElementById('V30e Img');
+		img.src = 'Images/Product/V30e Image2.webp'
+	}
+//y18i
+function Y18iimg1()
+	{
+		var img = document.getElementById('Y18i Img');
+		img.src = 'Images/Product/Y18i Image1.webp'
+	}
+function Y18iimg2()
+	{
+		var img = document.getElementById('Y18i Img');
+		img.src = 'Images/Product/Y18i Image2.webp'
+	}
+//y18
+function Y18img1()
+	{
+		var img = document.getElementById('Y18 Img');
+		img.src = 'Images/Product/Y18 Image1.webp'
+	}
+function Y18img2()
+	{
+		var img = document.getElementById('Y18 Img');
+		img.src = 'Images/Product/Y18 Image2.webp'
+	}
+//y18e
+function Y18eimg1()
+	{
+		var img = document.getElementById('Y18e Img');
+		img.src = 'Images/Product/Y18e Image1.webp'
+	}
+function Y18eimg2()
+	{
+		var img = document.getElementById('Y18e Img');
+		img.src = 'Images/Product/Y18e Image2.webp'
+	}
+//v30 pro
+function V30proimg1()
+	{
+		var img = document.getElementById('V30 Pro Img');
+		img.src = 'Images/Product/V30 Pro Image1.webp'
+	}
+function V30proimg2()
+	{
+		var img = document.getElementById('V30 Pro Img');
+		img.src = 'Images/Product/V30 Pro Image2.webp'
+	}
+//v30
+function V30img1()
+	{
+		var img = document.getElementById('V30 Img');
+		img.src = 'Images/Product/V30 Image1.webp'
+	}
+function V30img2()
+	{
+		var img = document.getElementById('V30 Img');
+		img.src = 'Images/Product/V30 Image2.webp'
+	}
+//V40e
+function V40eimg1()
+	{
+		var img = document.getElementById('V40e Img');
+		img.src = 'Images/Product/V40e Image1.webp'
+	}
+function V40eimg2()
+	{
+		var img = document.getElementById('V40e Img');
+		img.src = 'Images/Product/V40e Image2.webp'
+	}
+//X100
+function X100img1()
+	{
+		var img = document.getElementById('X100 Img');
+		img.src = 'Images/Product/X100 Image1.webp'
+	}
+function X100img2()
+	{
+		var img = document.getElementById('X100 Img');
+		img.src = 'Images/Product/X100 Image2.webp'
+	}
+//T3x 5G
+function T3x5gimg1()
+	{
+		var img = document.getElementById('T3x 5G Img');
+		img.src = 'Images/Product/T3x 5G Image1.webp'
+	}
+function T3x5gimg2()
+	{
+		var img = document.getElementById('T3x 5G Img');
+		img.src = 'Images/Product/T3x 5G Image2.webp'
+	}
+function T3x5gimg3()
+	{
+		var img = document.getElementById('T3x 5G Img');
+		img.src = 'Images/Product/T3x 5G Image3.webp'
+	}	
+//T3 5G
+function T35gimg1()
+	{
+		var img = document.getElementById('T3 5G Img');
+		img.src = 'Images/Product/T3 5G Image1.webp'
+	}
+function T35gimg2()
+	{
+		var img = document.getElementById('T3 5G Img');
+		img.src = 'Images/Product/T3 5G Image2.webp'
+	}
+//Y200e 5G
+function Y200e5gimg1()
+	{
+		var img = document.getElementById('Y200e 5G Img');
+		img.src = 'Images/Product/Y200e 5G Image1.webp'
+	}
+function Y200e5gimg2()
+	{
+		var img = document.getElementById('Y200e 5G Img');
+		img.src = 'Images/Product/Y200e 5G Image2.webp'
+	}
+//Y28 5G	
+function Y285gimg1()
+	{
+		var img = document.getElementById('Y28 5G Img');
+		img.src = 'Images/Product/Y28 5G Image1.webp'
+	}
+function Y285gimg2()
+	{
+		var img = document.getElementById('Y28 5G Img');
+		img.src = 'Images/Product/Y28 5G Image2.webp'
+	}
+//Y200 5G
+function Y2005gimg1()
+	{
+		var img = document.getElementById('Y200 5G Img');
+		img.src = 'Images/Product/Y200 5G Image1.webp'
+	}
+function Y2005gimg2()
+	{
+		var img = document.getElementById('Y200 5G Img');
+		img.src = 'Images/Product/Y200 5G Image2.webp'
+	}
+//V29
+function V29img1()
+{
+	var img = document.getElementById('V29 Img');
+	img.src = 'Images/Product/V29 Image1.webp'
+}
+function V29img2()
+	{
+		var img = document.getElementById('V29 Img');
+		img.src = 'Images/Product/V29 Image2.webp'
+	}
+function V29img3()
+	{
+		var img = document.getElementById('V29 Img');
+		img.src = 'Images/Product/V29 Image3.webp'
+	}
+//T2 Pro 5G	
+function T2pro5gimg1()
+	{
+		var img = document.getElementById('T2 Pro 5G Img');
+		img.src = 'Images/Product/T2 Pro 5G Image1.webp'
+	}
+function T2pro5gimg2()
+	{
+		var img = document.getElementById('T2 Pro 5G Img');
+		img.src = 'Images/Product/T2 Pro 5G Image2.webp'
+	}
+//V29 Pro
+function V29proimg1()
+	{
+		var img = document.getElementById('V29 Pro Img');
+		img.src = 'Images/Product/V29 Pro Image1.webp'
+	}
+function V29proimg2()
+	{
+		var img = document.getElementById('V29 Pro Img');
+		img.src = 'Images/Product/V29 Pro Image2.webp'
+	}
+//V29e 5G
+function V29e5gimg1()
+	{
+		var img = document.getElementById('V29e 5G Img');
+		img.src = 'Images/Product/V29e 5G Image1.webp'
+	}
+function V29e5gimg2()
+	{
+		var img = document.getElementById('V29e 5G Img');
+		img.src = 'Images/Product/V29e 5G Image2.webp'
+	}
+//Y17s	
+function Y17simg1()
+	{
+		var img = document.getElementById('Y17s Img');
+		img.src = 'Images/Product/Y17s Image1.webp'
+	}
+function Y17simg2()
+	{
+		var img = document.getElementById('Y17s Img');
+		img.src = 'Images/Product/Y17s Image2.webp'
+	}
+//Y36
+function Y36img1()
+	{
+		var img = document.getElementById('Y36 Img');
+		img.src = 'Images/Product/Y36 Image1.webp'
+	}
+function Y36img2()
+	{
+		var img = document.getElementById('Y36 Img');
+		img.src = 'Images/Product/Y36 Image2.webp'
+	}
+//Y27	
+function Y27img1()
+	{
+		var img = document.getElementById('Y27 Img');
+		img.src = 'Images/Product/Y27 Image1.webp'
+	}
+function Y27img2()
+	{
+		var img = document.getElementById('Y27 Img');
+		img.src = 'Images/Product/Y27 Image2.webp'
+	}
+//X90
+function X90img1()
+	{
+		var img = document.getElementById('X90 Img');
+		img.src = 'Images/Product/X90 Image1.webp'
+	}
+function X90img2()
+	{
+		var img = document.getElementById('X90 Img');
+		img.src = 'Images/Product/X90 Image2.webp'
+	}
+//T2X 5G
+function T2x5gimg1()
+	{
+		var img = document.getElementById('T2X 5G Img');
+		img.src = 'Images/Product/T2X 5G Image1.webp'
+	}
+function T2x5gimg2()
+	{
+		var img = document.getElementById('T2X 5G Img');
+		img.src = 'Images/Product/T2X 5G Image2.webp'
+	}
+function T2x5gimg3()
+	{
+		var img = document.getElementById('T2X 5G Img');
+		img.src = 'Images/Product/T2X 5G Image3.webp'
+	}
+//T2 5G	
+function T25gimg1()
+	{
+		var img = document.getElementById('T2 5G Img');
+		img.src = 'Images/Product/T2 5G Image1.webp'
+	}
+function T25gimg2()
+	{
+		var img = document.getElementById('T2 5G Img');
+		img.src = 'Images/Product/T2 5G Image2.webp'
+	}
+//Y100A
+function Y100aimg1()
+	{
+		var img = document.getElementById('Y100A Img');
+		img.src = 'Images/Product/Y100A Image1.webp'
+	}
+function Y100aimg2()
+	{
+		var img = document.getElementById('Y100A Img');
+		img.src = 'Images/Product/Y100A Image2.webp'
+	}
+function Y100aimg3()
+	{
+		var img = document.getElementById('Y100A Img');
+		img.src = 'Images/Product/Y100A Image3.webp'
+	}
+//Y02t
+function Y02timg1()
+	{
+		var img = document.getElementById('Y02t Img');
+		img.src = 'Images/Product/Y02t Image1.webp'
+	}
+function Y02timg2()
+	{
+		var img = document.getElementById('Y02t Img');
+		img.src = 'Images/Product/Y02t Image2.webp'
+	}
+//V27 Pro
+function V27proimg1()
+	{
+		var img = document.getElementById('V27 Pro Img');
+		img.src = 'Images/Product/V27 Pro Image1.webp'
+	}
+function V27proimg2()
+	{
+		var img = document.getElementById('V27 Pro Img');
+		img.src = 'Images/Product/V27 Pro Image2.webp'
+	}
+//V27
+function V27img1()
+	{
+		var img = document.getElementById('V27 Img');
+		img.src = 'Images/Product/V27 Image1.webp'
+	}
+function V27img2()
+	{
+		var img = document.getElementById('V27 Img');
+		img.src = 'Images/Product/V27 Image2.webp'
+	}
+//Y56 5G
+function Y565gimg1()
+	{
+		var img = document.getElementById('Y56 5G Img');
+		img.src = 'Images/Product/Y56 5G Image1.webp'
+	}
+function Y565gimg2()
+	{
+		var img = document.getElementById('Y56 5G Img');
+		img.src = 'Images/Product/Y56 5G Image2.webp'
+	}
+//Y100	
+function Y100img1()
+	{
+		var img = document.getElementById('Y100 Img');
+		img.src = 'Images/Product/Y100 Image1.webp'
+	}
+function Y100img2()
+	{
+		var img = document.getElementById('Y100 Img');
+		img.src = 'Images/Product/Y100 Image2.webp'
+	}
+function Y100img3()
+	{
+		var img = document.getElementById('Y100 Img');
+		img.src = 'Images/Product/Y100 Image3.webp'
+	}
+//Y02
+function Y02img1()
+	{
+		var img = document.getElementById('Y02 Img');
+		img.src = 'Images/Product/Y02 Image1.webp'
+	}
+function Y02img2()
+	{
+		var img = document.getElementById('Y02 Img');
+		img.src = 'Images/Product/Y02 Image2.webp'
+	}
+//Y16
+function Y16img1()
+	{
+		var img = document.getElementById('Y16 Img');
+		img.src = 'Images/Product/Y16 Image1.webp'
+	}
+function Y16img2()
+	{
+		var img = document.getElementById('Y16 Img');
+		img.src = 'Images/Product/Y16 Image2.webp'
+	}
+//Y22
+function Y22img1()
+	{
+		var img = document.getElementById('Y22 Img');
+		img.src = 'Images/Product/Y22 Image1.webp'
+	}
+function Y22img2()
+	{
+		var img = document.getElementById('Y22 Img');
+		img.src = 'Images/Product/Y22 Image2.webp'
+	}
+//Y35
+function Y35img1()
+	{
+		var img = document.getElementById('Y35 Img');
+		img.src = 'Images/Product/Y35 Image1.webp'
+	}
+function Y35img2()
+	{
+		var img = document.getElementById('Y35 Img');
+		img.src = 'Images/Product/Y35 Image2.webp'
+	}
+//T1x
+function T1ximg1()
+	{
+		var img = document.getElementById('T1x Img');
+		img.src = 'Images/Product/T1x Image1.webp'
+	}
+function T1ximg2()
+	{
+		var img = document.getElementById('T1x Img');
+		img.src = 'Images/Product/T1x Image2.webp'
+	}
+//T1	
+function T1img1()
+	{
+		var img = document.getElementById('T1 Img');
+		img.src = 'Images/Product/T1 Image1.webp'
+	}
+function T1img2()
+	{
+		var img = document.getElementById('T1 Img');
+		img.src = 'Images/Product/T1 Image2.webp'
+	}
+function T1img3()
+	{
+		var img = document.getElementById('T1 Img');
+		img.src = 'Images/Product/T1 Image3.webp'
+	}
+//T1 Pro 5G
+function T1pro5gimg1()
+	{
+		var img = document.getElementById('T1 Pro 5G Img');
+		img.src = 'Images/Product/T1 Pro 5G Image1.webp'
+	}
+function T1pro5gimg2()
+	{
+		var img = document.getElementById('T1 Pro 5G Img');
+		img.src = 'Images/Product/T1 Pro 5G Image2.webp'
+	}
+//T1 5G
+function T15gimg1()
+	{
+		var img = document.getElementById('T1 5G Img');
+		img.src = 'Images/Product/T1 5G Image1.webp'
+	}
+function T15gimg2()
+	{
+		var img = document.getElementById('T1 5G Img');
+		img.src = 'Images/Product/T1 5G Image2.webp'
+	}
+//Y75 5G	
+function Y755gimg1()
+	{
+		var img = document.getElementById('Y75 5G Img');
+		img.src = 'Images/Product/Y75 5G Image1.webp'
+	}
+function Y755gimg2()
+	{
+		var img = document.getElementById('Y75 5G Img');
+		img.src = 'Images/Product/Y75 5G Image2.webp'
+	}	
+	
+// all models
+function allmodels()
+{
+	var allmodels = document.getElementById('all-models-details')
+	var xseries = document.getElementById('x-series-details')
+	var vseries = document.getElementById('v-series')
+	var yseries = document.getElementById('y-series')
+	var tseries = document.getElementById('t-series')
+	var accessories = document.getElementById('accessories')
+	allmodels.style.display='block'
+	xseries.style.display='none'
+	vseries.style.display='none'
+	yseries.style.display='none'
+	tseries.style.display='none'
+	accessories.style.display='none'
+}
+//xseries
+function xseries()
+{
+	var allmodels = document.getElementById('all-models-details')
+	var xseries = document.getElementById('x-series-details')
+	var vseries = document.getElementById('v-series')
+	var yseries = document.getElementById('y-series')
+	var tseries = document.getElementById('t-series')
+	var accessories = document.getElementById('accessories')
+	allmodels.style.display='none'
+	xseries.style.display='block'
+	vseries.style.display='none'
+	yseries.style.display='none'
+	tseries.style.display='none'
+	accessories.style.display='none'
+}
+//v series
+function vseries()
+{
+	var allmodels = document.getElementById('all-models-details')
+	var xseries = document.getElementById('x-series-details')
+	var vseries = document.getElementById('v-series')
+	var yseries = document.getElementById('y-series')
+	var tseries = document.getElementById('t-series')
+	var accessories = document.getElementById('accessories')
+	allmodels.style.display='none'
+	xseries.style.display='none'
+	vseries.style.display='block'
+	yseries.style.display='none'
+	tseries.style.display='none'
+	accessories.style.display='none'
+}
+// y series
+function yseries()
+{
+	var allmodels = document.getElementById('all-models-details')
+	var xseries = document.getElementById('x-series-details')
+	var vseries = document.getElementById('v-series')
+	var yseries = document.getElementById('y-series')
+	var tseries = document.getElementById('t-series')
+	var accessories = document.getElementById('accessories')
+	allmodels.style.display='none'
+	xseries.style.display='none'
+	vseries.style.display='none'
+	yseries.style.display='block'
+	tseries.style.display='none'
+	accessories.style.display='none'
+}
+//t series
+function tseries()
+{
+	var allmodels = document.getElementById('all-models-details')
+	var xseries = document.getElementById('x-series-details')
+	var vseries = document.getElementById('v-series')
+	var yseries = document.getElementById('y-series')
+	var tseries = document.getElementById('t-series')
+	var accessories = document.getElementById('accessories')
+	allmodels.style.display='none'
+	xseries.style.display='none'
+	vseries.style.display='none'
+	yseries.style.display='none'
+	tseries.style.display='block'
+	accessories.style.display='none'
+}
+//accessories
+function accessories()
+{
+	var allmodels = document.getElementById('all-models-details')
+	var xseries = document.getElementById('x-series-details')
+	var vseries = document.getElementById('v-series')
+	var yseries = document.getElementById('y-series')
+	var tseries = document.getElementById('t-series')
+	var accessories = document.getElementById('accessories')
+	allmodels.style.display='none'
+	xseries.style.display='none'
+	vseries.style.display='none'
+	yseries.style.display='none'
+	tseries.style.display='none'
+	accessories.style.display='block'
+}
